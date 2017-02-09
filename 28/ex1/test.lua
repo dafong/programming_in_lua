773b056d686baba0821842a2c19375381936f25a
范鑫磊 <xinlei.fan@redatoms.com>
2017-02-09 18:33:45 +0800

    up

---
+++ 28/ex1/test.lua
@@ -0,0 +1,11 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: xinlei.fan
+-- Date: 17/2/9
+-- Time: 下午6:29
+-- To change this template use File | Settings | File Templates.
+--
+filter({1,2,3},function(v)
+   return v+1
+end)
+
